b4e830e8d515285fe8050d279b1a90e654731339
lacoski <123xuzu@gmail.com>
2019-12-26 10:39:47 +0700

    update repo

---
+++ src/authenticate/users/urls.py
@@ -0,0 +1,15 @@
+"""USER URL
+"""
+from django.contrib import admin
+from django.urls import path
+from rest_framework.authtoken import views
+from rest_framework_simplejwt import views as jwt_views
+
+from .views import HelloView
+
+urlpatterns = [
+    # path('api-token-auth/', views.obtain_auth_token),
+    path('hello_view/', HelloView.as_view()),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+]
